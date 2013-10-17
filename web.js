e208873f6ffea3728f9dd8aebe24f2fef7879f1e
slavkur <slavkur2013@gmail.com>
2013-10-17 19:39:24 +0300

    creating puzzle app

---
+++ web.js
@@ -0,0 +1,23 @@
+// Initiate requirements
+var express = require("express");
+
+// Create app using express
+// Set ejs as template framework
+var app = express();
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + "/views");
+app.set('view engine', 'ejs');
+
+// Set the port and listen
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+
+app.get('/heroku', function(request, response) {
+  response.render('heroku');
+});
+
+app.get('/phonelist', function(request, response) {
+  response.render('phonelist');
+});
